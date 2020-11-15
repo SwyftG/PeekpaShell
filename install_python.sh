b52e88a562fcec0f86784d1b0628536deb256e50
lg1889 <lg1889@nyu.edu>
2020-11-15 16:12:59 +0900

    install_python

--- install_python.sh
+++ install_python.sh
@@ -1,4 +1,29 @@
 #! /bin/bash
-echo "############### Welcome to Peekpa.com ###############"
-echo "##### This script is to install Python 3.6 #####"
-echo "Start..."
+startTime=`date +%Y%m%d-%H:%M:%S`
+startTime_s=`date +%s`
+
+echo "#################### Welcome to Peekpa.com ####################"
+echo "##########                                           ##########"
+echo "##########            /install_python.sh/            ##########"
+echo "##########                                           ##########"
+echo "##########     This script is to install Python3     ##########"
+echo "##########            on CentOS 7.7 64bit            ##########"
+echo "##########                                           ##########"
+echo "##########                Written By                 ##########"
+echo "##########                  Peekpa                   ##########"
+echo "##########                                           ##########"
+echo "############################ ENJOY ############################"
+
+echo ">>>>>>>>>>>>>>>> Script Start >>>>>>>>>>>>>>>>"
+
+sudo yum install -y python3
+
+echo ">>>>>>>>>>>>>>>> Script Done >>>>>>>>>>>>>>>>>"
+
+endTime=`date +%Y%m%d-%H:%M:%S`
+endTime_s=`date +%s`
+
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "Start at -->> $startTime"
+echo "End at -->> $endTime"
+echo "Total -->> $sumTime seconds"
